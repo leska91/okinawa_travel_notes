7ac682aceff2f4a81830fdeaaa21b76de4df6385
leska91 <mayu1101u@gmail.com>
2021-03-02 13:08:42 +0900

    編集〜更新

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -11,6 +11,20 @@ class RecordsController < ApplicationController
     Record.create(record_params)
   end
 
+  def destroy
+    record = Record.find(params[:id])
+    record.destroy
+  end
+
+  def edit
+    @record = Record.find(params[:id])
+  end
+
+  def update
+    record = Record.find(params[:id])
+    record.update(record_params)
+  end
+
   private
   def record_params
     params.require(:record).permit(:text, :area_id, :image)
