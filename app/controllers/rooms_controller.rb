599f322555d5fcb459dc84f17e88ddbec1e0c9aa
leska91 <mayu1101u@gmail.com>
2021-03-08 18:50:38 +0900

    chatroom作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,7 +11,7 @@ class RoomsController < ApplicationController
   def create
     @room = Room.new(room_params)
     if @room.save
-      redirect_to 
+      redirect_to root_path
     else
       render :new
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_082116) do
+ActiveRecord::Schema.define(version: 2021_03_08_020758) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -71,9 +71,9 @@ ActiveRecord::Schema.define(version: 2021_03_08_082116) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
