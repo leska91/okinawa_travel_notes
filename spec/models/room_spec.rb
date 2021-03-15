65c9d8359692316f245bd52d1997ae427beb6f8e
leska91 <mayu1101u@gmail.com>
2021-03-15 15:12:12 +0900

    room_model_test

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'RoomCreate' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it 'nameに情報が入力されていれば保存できる' do
+      expect(@room).to be_valid
+    end
+
+    it 'nameが空の場合は保存できない' do
+      @room.name = ''
+      @room.valid?
+      expect(@room.errors.full_messages).to include("Name can't be blank")
+    end
+  end
 end
